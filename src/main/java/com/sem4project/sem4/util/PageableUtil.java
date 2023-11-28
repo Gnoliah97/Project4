f7250d286a10f171a393f3c8de4fa24a009d33f6
Bách Trần <bachtran10013@gmail.com>
2023-11-28 19:03:28 +0700

    update

--- src/main/java/com/sem4project/sem4/util/PageableUtil.java
+++ src/main/java/com/sem4project/sem4/util/PageableUtil.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 
 public class PageableUtil {
     public static Pageable calculatePageable(long count, int pageNumber, int pageSize, String sortBy, String sortType) {
-        int maxPage = (int) Math.ceil((double) count / pageSize) - 1;
+        int maxPage = (int) Math.ceil((double) count / pageSize);
         pageNumber--;
         pageNumber = Math.max(0, pageNumber);
         pageNumber = Math.min(pageNumber, maxPage);
